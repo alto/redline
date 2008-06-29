76c55d582d85fc629349fe9a45578d98d0a28ce7
Alto <boettger@mt7.de>
2008-06-29 15:48:46 +0200

    added deploying

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+
+default_run_options[:pty] = true
+
+set :application, "redline"
+set :repository,  "git@github.com:alto/redline.git"
+set :branch,      "origin/master"
+
+set :deploy_to, "/home/alto/apps/#{application}"
+
+set :scm, :git
+set :user, "alto"
+
+# ssh_options[:keys] = [ENV['HOME'] + "/.ssh/id_dsa", ENV['HOME'] + "/.ssh/id_rsa"]
+
+role :app, "redline.42towels.com"
+role :web, "redline.42towels.com"
+role :db,  "redline.42towels.com", :primary => true
+
+after 'deploy:update_code', :set_symlinks
+desc "Setting symlinks for everything"
+task :set_symlinks do
+  run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  run "ln -s #{shared_path}/data #{release_path}/public/data"
+end
--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,4 +1,4 @@
 ExceptionNotifier.exception_recipients = %w(boettger@mt7.de)
 
-ExceptionNotifier.sender_address = %("Exception Notifier" <exception@todoode.42towels.com>)
-ExceptionNotifier.email_prefix = "[ERROR] todoode "
+ExceptionNotifier.sender_address = %("Exception Notifier" <exception@redline.42towels.com>)
+ExceptionNotifier.email_prefix = "[ERROR] redline "
