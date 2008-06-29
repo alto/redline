5874b0ca62c50330b52477eafb7fd63f32a1c7bf
Alto <boettger@mt7.de>
2008-06-29 18:14:39 +0200

    bugfix

--- db/migrate/20080629160803_user_add_url_slug.rb
+++ db/migrate/20080629160803_user_add_url_slug.rb
@@ -1,7 +1,7 @@
 class UserAddUrlSlug < ActiveRecord::Migration
   def self.up
     add_column :users, :url_slug, :string
-    User.find(:all).each {|user| u.save!}
+    User.find(:all).each {|user| user.save!}
   end
 
   def self.down
